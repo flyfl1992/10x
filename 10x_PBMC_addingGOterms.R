2c0fce52912da63e2f9462887001f0f3f81588f5
Zachary Lewis <zachary_lewis@brown.edu>
2017-03-28 12:23:20 -0400

    added GO search to main script

--- 10x_PBMC_addingGOterms.R
+++ 10x_PBMC_addingGOterms.R
@@ -9,6 +9,7 @@ library(Seurat) # http://satijalab.org/seurat/
 # From cran
 library(tidyverse)
 library(Matrix)
+library(stringr)
 
 ##############################
 # Reading data into CellRangerR:
@@ -30,81 +31,142 @@ gbm <- load_cellranger_matrix(pipestance_path) #gbm has 2700 cells and 32,738 ge
 # fData(gbm) # data frame of genes
 # pData(gbm) # data frame of cell barcodes
 
-###########################
-# reading data into Seurat:
-###########################
+################
+# Searching for a GO term and its ancestors
+################
 
-#relative filepath for matrices
-filepath <- "./outs/filtered_gene_bc_matrices/hg19/"
-pbmc.data <- Read10X(filepath)
+go_map <- read_tsv("go_map.tsv")
 
-#In Seurat, the following command is used to read raw data into S4 class
-#pbmc <- new("seurat", raw.data = pbmc.data)
+#go_map[go_map$go_id == "GO:1903506", ]
 
-#################
-# Loading GO Terms
-#################
+#transcription factor activity, protein binding
+# can be accessed as follows:
+# go_map[go_map$go_id == "GO:0000988", ]
 
-#import list of gene IDs from 10x data (Note that this is not necessary if you have cloned the repo)
-# gene_names2 <- read_delim("genes.tsv", delim = "\t",col_names = c("Ensembl","ID"))
+
+#' Returns ENSEMBL IDs from a GO term search
+#' 
+#' @param Character indicating a go term, eg "GO:0003712"
+#' @return A vector of ENSEMBL IDs
+#' @export
+id_search = function( go_term ){
+  
+  row_return = go_map %>%
+    filter(str_detect(go_id, go_term)) 
+  
+  row_return = row_return[! duplicated(row_return$ensembl_gene_id), ]
+  
+  id_vector = as.vector(row_return$ensembl_gene_id)
+  
+  return(id_vector)
+}
+
+#store your search results in a variable, 
+# such as: 
+transcription_factor_vector <- id_search("GO:0000988")
+
+##################
+#Subset by GO_Term
+##################
+
+# Need to make sure that the ENSEMBL ids from GO_Search are actually in the gbm matrix
+# `transcription_factor_vector` represents the results of the id_search() function
+transcription_factor_vector2 = transcription_factor_vector[ transcription_factor_vector %in% fData(gbm)$id ]
+#then subset by these GO terms
+subset_by_GO_term <-gbm[c(transcription_factor_vector2),]
+
+
+#can use fData, pData and exprs functions on this subset
+table(exprs(subset_by_GO_term)[1,]) #summary
+table(fData(subset_by_GO_term)[1:20,])
+
+#the expression matrix would be accessed with:
+expression_subset <- exprs(subset_by_GO_term)
+head(expression_subset)
+
+###
+# Next step is to re-run sequencing analysis using subsetted data...
+
+
+
+
+
+
+# ###########################
+# # reading data into Seurat:
+# ###########################
 # 
-# write.csv(gene_names2[,1],file = "genewrite",row.names=FALSE) #write Ensembl IDs to file
-
-#use Retrieve/ID mapping to convert from Ensembl to UniprotKB
-# http://www.uniprot.org/uploadlists/ # paste in list of genes, or upload (may have to remove quotation marks first)
-#  filter by "Reviewed / Swiss-Prot"
-#   download results as a tab separated file
-#    rename downloaded file to "uniprot.tab"
-
-uniprot<-read_delim("uniprot.tab", delim = "\t")
-summary(uniprot)
-#rename first two columns
-names(uniprot)[1:2] = c("id","isomap")
-head(uniprot)
-#remove duplicated Ensembl IDs
-#(note: coarse way to do this, should investigate why some IDs are duplicated)
-uniprot[! duplicated(uniprot$id), ] -> uniprot.noDup
-head( uniprot.noDup )
-
-##################
-## Relating GO terms to PBMC Data
-##################
-
-#relational database from the genes database to the uniprot.noDup
-gene_annotations <- 
-	left_join( fData(gbm), uniprot.noDup, by = "id") %>% 
-	as_tibble()
-
-#the problem is that the CellrangerR package doesn't support addition of metadata
-#  need to instead use an non-sparse matrix 
-
-#I think that as.matrix parses the Seurat pbmc.data
-denseMatrix <- as.matrix( pbmc.data )
-#however,
-names( denseMatrix )
-# NULL
-
-#instead tried to join this database to the raw reads parsed by Seurat
-pbmc.tibble <- as_tibble( denseMatrix )
-join_reads <- left_join( gene_annotations, pbmc.tibble, by = NULL ) #throwing error, even though there should be a shared Gene ID column
-#using as.matrix(pbmc.data) seems to elminate the gene IDs present in pbmc.data
-
-head(pbmc.tibble)
-head(pbmc.data)
-head(gene_annotations)
-
-#In Seurat there is a function to add metadata, perhaps could use that
-#AddMetaData adds columns to object@data.info, and is a great place to stash QC stats
-#mito.genes <- grep("^MT-", rownames(pbmc@data), value = T)
-#percent.mito <- colSums(expm1(pbmc@data[mito.genes, ]))/colSums(expm1(pbmc@data))
-pbmc <- new("seurat", raw.data = pbmc.data)
-#pbmc <- AddMetaData(pbmc, percent.mito, "percent.mito")
-
-#can potentially use the AddMetaData function from Seurat to add columns
-pbmc.join <- AddMetaData(pbmc, gene_annotations)
-#no errors, but
-names(pbmc.join)
-#NULL
+# #relative filepath for matrices
+# filepath <- "./outs/filtered_gene_bc_matrices/hg19/"
+# pbmc.data <- Read10X(filepath)
+# 
+# #In Seurat, the following command is used to read raw data into S4 class
+# #pbmc <- new("seurat", raw.data = pbmc.data)
+# 
+# #################
+# # Loading GO Terms
+# #################
+# 
+# #import list of gene IDs from 10x data (Note that this is not necessary if you have cloned the repo)
+# # gene_names2 <- read_delim("genes.tsv", delim = "\t",col_names = c("Ensembl","ID"))
+# # 
+# # write.csv(gene_names2[,1],file = "genewrite",row.names=FALSE) #write Ensembl IDs to file
+# 
+# #use Retrieve/ID mapping to convert from Ensembl to UniprotKB
+# # http://www.uniprot.org/uploadlists/ # paste in list of genes, or upload (may have to remove quotation marks first)
+# #  filter by "Reviewed / Swiss-Prot"
+# #   download results as a tab separated file
+# #    rename downloaded file to "uniprot.tab"
+# 
+# uniprot<-read_delim("uniprot.tab", delim = "\t")
+# summary(uniprot)
+# #rename first two columns
+# names(uniprot)[1:2] = c("id","isomap")
+# head(uniprot)
+# #remove duplicated Ensembl IDs
+# #(note: coarse way to do this, should investigate why some IDs are duplicated)
+# uniprot[! duplicated(uniprot$id), ] -> uniprot.noDup
+# head( uniprot.noDup )
+# 
+# ##################
+# ## Relating GO terms to PBMC Data
+# ##################
+# 
+# #relational database from the genes database to the uniprot.noDup
+# gene_annotations <- 
+# 	left_join( fData(gbm), uniprot.noDup, by = "id") %>% 
+# 	as_tibble()
+# 
+# #the problem is that the CellrangerR package doesn't support addition of metadata
+# #  need to instead use an non-sparse matrix 
+# 
+# #I think that as.matrix parses the Seurat pbmc.data
+# denseMatrix <- as.matrix( pbmc.data )
+# #however,
+# names( denseMatrix )
+# # NULL
+# 
+# #instead tried to join this database to the raw reads parsed by Seurat
+# pbmc.tibble <- as_tibble( denseMatrix )
+# join_reads <- left_join( gene_annotations, pbmc.tibble, by = NULL ) #throwing error, even though there should be a shared Gene ID column
+# #using as.matrix(pbmc.data) seems to elminate the gene IDs present in pbmc.data
+# 
+# head(pbmc.tibble)
+# head(pbmc.data)
+# head(gene_annotations)
+# 
+# #In Seurat there is a function to add metadata, perhaps could use that
+# #AddMetaData adds columns to object@data.info, and is a great place to stash QC stats
+# #mito.genes <- grep("^MT-", rownames(pbmc@data), value = T)
+# #percent.mito <- colSums(expm1(pbmc@data[mito.genes, ]))/colSums(expm1(pbmc@data))
+# pbmc <- new("seurat", raw.data = pbmc.data)
+# #pbmc <- AddMetaData(pbmc, percent.mito, "percent.mito")
+# 
+# #can potentially use the AddMetaData function from Seurat to add columns
+# pbmc.join <- AddMetaData(pbmc, gene_annotations)
+# #no errors, but
+# names(pbmc.join)
+# #NULL
 
 
 
--- 10x_PBMC_dataAnalysis_CellRangerR.R
+++ 10x_PBMC_dataAnalysis_CellRangerR.R
@@ -5,14 +5,14 @@ source("http://s3-us-west-2.amazonaws.com/10x.files/code/rkit-install-1.1.0.R")
 
 library(cellrangerRkit)
 
-setwd("~/Documents/Dunn_Lab/10x/")
-
-packageVersion("cellrangerRkit")
+#packageVersion("cellrangerRkit")
 
 
 library(devtools)
 
-pipestance_path <- "/Users/Zack/Documents/Dunn_Lab/10x"
+#specifying file path
+pipestance_path <- getwd()
+
 setwd(pipestance_path)
 download_sample(sample_name="pbmc3k",sample_dir=pipestance_path, 
                 host="https://s3-us-west-2.amazonaws.com/10x.files/samples/cell/") 
@@ -22,7 +22,6 @@ analysis_results <- load_cellranger_analysis_results(pipestance_path)
 exprs(gbm) # expression matrix
 fData(gbm) # data frame of genes
 pData(gbm) # data frame of cell barcodes
-
 
 #t-SNE projection and plot the cells colored by UMI counts
 # generates a t-SNE projection where each cell is colored by log10 of UMI counts.
@@ -51,9 +50,7 @@ genes <- c("CD79A","NKG7","CD3D","CST3","CD8A","PF4")
 pca_proj <- analysis_results$pca 
 visualize_gene_markers(gbm_log,genes,pca_proj[c("PC.1","PC.2")],limits=c(0,1.5))
 
-
 #visualizing clusters at different kmeans clustering levels:
-
 n_clu <- 2:10
 km_res <- analysis_results$kmeans # load pre-computed kmeans results
 clu_res <- sapply(n_clu, function(x) km_res[[paste(x,"clusters",sep="_")]]$Cluster) 
@@ -84,13 +81,10 @@ write_cluster_specific_genes(prioritized_genes, output_folder, n_genes=10)
 # genes are rows, cells are columns. Top 3 DE genes for each cluster are displayed
 gbm_pheatmap(log_gene_bc_matrix(gbm), prioritized_genes, cells_to_plot, n_genes=5, colour=example_col, limits=c(-1,2))
 
-
 #from your bait genes, make calls as to what cell type each cluster represents 
 # and output composition of entire population
 cell_composition(cluster_result$Cluster,
                  anno=c("monocytes","T cells","NK cells","megakaryocytes","B cells"))
-
-
 
 #Subsetting data
 # examples of how to subset:
@@ -98,6 +92,11 @@ cell_composition(cluster_result$Cluster,
 # subset_by_gene_id <- gbm["ENSG00000167286",]
 # subset_by_gene_symbol <- gbm[which(fData(gbm)$symbol == 'CD3D'),]
 # subset_by_cell_and_gene <- gbm["ENSG00000167286", c("AAACATACAACCAC-1", "AAACATTGAGCTAC-1")]
+
+# #examining expression data over cells for a certain gene
+# exprs(gbm[6,400:500])
+# fData(gbm[1,1])
+# 
 
 
 #Subset by GO_Term
@@ -108,13 +107,17 @@ transcription_factor_vector2 = transcription_factor_vector[ transcription_factor
 subset_by_GO_term <-gbm[c(transcription_factor_vector2),]
 
 
+#can use fData, pData and exprs functions on this subset
+table(exprs(subset_by_GO_term)[1,]) #summary
+table(fData(subset_by_GO_term)[1:20,])
+
+#the expression matrix would be accessed with:
+expression_subset <- exprs(subset_by_GO_term)
+head(expression_subset)
+
 ###
 # Next step is to re-run sequencing analysis using subsetted data...
 
 
-# #examining expression data over cells for a certain gene
-# exprs(gbm[6,400:500])
-# fData(gbm[1,1])
-# 
 
 
